78381920579138c1a12fad53927260f95485a692
Grace Nieboer <grace.nieboer@outlook.com>
2023-12-08 12:30:13 -0500

    create shared container component and use it in the home screen that just says welcome currently

---
+++ components/screens/Home.tsx
@@ -0,0 +1,14 @@
+import { FunctionComponent } from "react";
+import { Container } from "../shared/container";
+import { Text } from "react-native";
+
+
+const Home: FunctionComponent = () => {
+
+    return (
+        <Container>
+            <Text>WELCOME</Text>
+        </Container>
+    )
+    
+}
