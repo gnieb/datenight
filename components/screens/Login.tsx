d801bf99eef8d6493a935d65b839c3ba97b8d851
Grace Nieboer <grace.nieboer@outlook.com>
2023-12-14 18:39:02 -0500

    base for login, need to build the form

---
+++ components/screens/Login.tsx
@@ -0,0 +1,18 @@
+import { FunctionComponent } from "react";
+import { Container } from "../shared/container";
+import SafeAreaView from "react-native-safe-area-view";
+
+
+export const Login:FunctionComponent= () => {
+    return (
+        <Container>
+            <SafeAreaView>
+            {/* FORM */}
+            </SafeAreaView>
+        </Container>
+    )
+}
+
+
+
+
