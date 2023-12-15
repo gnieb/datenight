8cdbaface1bddab30c6cf860ba6abc6c2c3fa5ea
Grace Nieboer <grace.nieboer@outlook.com>
2023-12-15 09:39:01 -0500

    install formik and plug basic into Signup

--- components/screens/Signup.tsx
+++ components/screens/Signup.tsx
@@ -1,11 +1,27 @@
 import { FunctionComponent } from "react";
 import { Container } from "../shared/container";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, View, Text, TextInput, Button } from "react-native";
+import { Formik } from 'formik';
 
 
 export const Signup:FunctionComponent = () => {
     return (
         <>
+        <Formik
+     initialValues={{ email: '' }}
+     onSubmit={values => console.log(values)}
+   >
+     {({ handleChange, handleBlur, handleSubmit, values }) => (
+       <View>
+         <TextInput
+           onChangeText={handleChange('email')}
+           onBlur={handleBlur('email')}
+           value={values.email}
+         />
+         <Button onPress={() => handleSubmit} title="Submit" />
+       </View>
+     )}
+   </Formik>
         </>
     )
 } 
