8ee0ebedf1fd6fa3c05ddab978f6b883487af9c1
Grace Nieboer <grace.nieboer@outlook.com>
2023-12-15 09:44:28 -0500

    install Yup validation and set up signupSchema and initial values for formik form

--- components/screens/Signup.tsx
+++ components/screens/Signup.tsx
@@ -2,14 +2,25 @@ import { FunctionComponent } from "react";
 import { Container } from "../shared/container";
 import { SafeAreaView, View, Text, TextInput, Button } from "react-native";
 import { Formik } from 'formik';
+import * as Yup from 'yup';
 
+
+const signupSchema = Yup.object().shape({
+    firstName: Yup.string().required("Please add your first name here"),
+    lastName:Yup.string().required("Please add your last name"),
+    email:Yup.string().email("Not a valid email address").required("Please add your email to get started!")
+})
 
 export const Signup:FunctionComponent = () => {
     return (
         <>
         <Formik
-     initialValues={{ email: '' }}
+     initialValues={{ 
+        firstName:'',
+        lastName:'',
+        email: '' }}
      onSubmit={values => console.log(values)}
+     validationSchema={signupSchema}
    >
      {({ handleChange, handleBlur, handleSubmit, values }) => (
        <View>
