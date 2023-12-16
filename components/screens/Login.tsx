7a33b4b5b3511a38265adb93939fd3b4457d9329
Grace Nieboer <grace.nieboer@outlook.com>
2023-12-16 14:45:36 -0500

    add password regex and initial value to new user form signup component

--- components/screens/Login.tsx
+++ components/screens/Login.tsx
@@ -1,13 +1,39 @@
 import { FunctionComponent } from "react";
 import { Container } from "../shared/container";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, View, Text, TextInput, Button } from "react-native";
+import {Formik } from 'formik';
+import * as Yup from 'yup';
 
+
+const loginSchema = Yup.object().shape({
+    email: Yup.string().email("Invalid email").required("We need your email to log you in"),
+    password: Yup.string().required("Invlaid password")
+
+})
 
 export const Login:FunctionComponent= () => {
     return (
         <Container>
             <SafeAreaView>
-            {/* FORM */}
+                <Formik
+                    initialValues={{ 
+                        firstName:'',
+                        lastName:'',
+                        email: '' }}
+                    onSubmit={values => console.log(values)}
+                    validationSchema={loginSchema}
+                >
+                {({ handleChange, handleBlur, handleSubmit, values }) => (
+                <View>
+                    <TextInput
+                    onChangeText={handleChange('email')}
+                    onBlur={handleBlur('email')}
+                    value={values.email}
+                    />
+                    <Button onPress={() => handleSubmit} title="Submit" />
+                </View>
+                )}
+                </Formik>
             </SafeAreaView>
         </Container>
     )
--- components/screens/Signup.tsx
+++ components/screens/Signup.tsx
@@ -8,31 +8,38 @@ import * as Yup from 'yup';
 const signupSchema = Yup.object().shape({
     firstName: Yup.string().required("Please add your first name here"),
     lastName:Yup.string().required("Please add your last name"),
-    email:Yup.string().email("Not a valid email address").required("Please add your email to get started!")
+    email:Yup.string().email("Not a valid email address").required("Please add your email to get started!"),
+    password: Yup.string().required()
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+    "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"),
+    // use the regex???? 
 })
 
 export const Signup:FunctionComponent = () => {
     return (
-        <>
-        <Formik
-     initialValues={{ 
-        firstName:'',
-        lastName:'',
-        email: '' }}
-     onSubmit={values => console.log(values)}
-     validationSchema={signupSchema}
-   >
-     {({ handleChange, handleBlur, handleSubmit, values }) => (
-       <View>
-         <TextInput
-           onChangeText={handleChange('email')}
-           onBlur={handleBlur('email')}
-           value={values.email}
-         />
-         <Button onPress={() => handleSubmit} title="Submit" />
-       </View>
-     )}
-   </Formik>
-        </>
+        <Container>
+        <SafeAreaView>
+            <Formik
+            initialValues={{ 
+                firstName:'',
+                lastName:'',
+                email: '', 
+                password:'' }}
+            onSubmit={values => console.log(values)}
+            validationSchema={signupSchema}
+            >
+            {({ handleChange, handleBlur, handleSubmit, values }) => (
+            <View>
+                <TextInput
+                onChangeText={handleChange('email')}
+                onBlur={handleBlur('email')}
+                value={values.email}
+                />
+                <Button onPress={() => handleSubmit} title="Submit" />
+            </View>
+            )}
+            </Formik>
+        </SafeAreaView>
+    </Container>
     )
 } 
