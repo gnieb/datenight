81ddec3f7377c637c7d93490a6d7e09cb92a89a1
Grace Nieboer <grace.nieboer@outlook.com>
2023-12-22 23:32:51 -0500

    create outside rootnav and begin to create the gateway that will use the boolean to choose with root nav it will use

--- App.tsx
+++ App.tsx
@@ -3,12 +3,26 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { RootNav } from './components/navigation/RootNav';
 import SafeAreaView from 'react-native-safe-area-view';
+import { useAuth, AuthProvider } from './components/context/AuthContext';
+import { FunctionComponent } from 'react';
 
 const App: React.FC = () => {
   return (
-    <RootNav />
+    <AuthProvider>
+      <RootNav />
+    </AuthProvider>
   );
+};
+
+
+const Gateway:FunctionComponent = () => {
+
+  const {authState} = useAuth()
+
+  return authState? <RootNav/> :  
+
 }
+
 
 const styles = StyleSheet.create({
   container: {
