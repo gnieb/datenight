9360a1195b5e46f9cde4eea82ddf0b0c75ed6c2f
Grace Nieboer <grace.nieboer@outlook.com>
2024-01-02 23:24:20 -0500

    add the profile form and add state to open and close the form.. need to possbily use a react modal or pop up for the form

--- components/screens/Profile.tsx
+++ components/screens/Profile.tsx
@@ -1,23 +1,42 @@
-import { FunctionComponent } from "react";
-import { SafeAreaView, Text, Pressable, StyleSheet } from "react-native";
+import { FunctionComponent, useState, useEffect } from "react";
+import { SafeAreaView, Text, Pressable, StyleSheet, View } from "react-native";
 import { Container } from "../shared/container";
 import { useAuth } from "../context/AuthContext";
 
 
 const Profile:FunctionComponent = () => {
 
-    const {onLogout, authState, user} = useAuth()
+    const {onLogout, user} = useAuth();
+    const [connectOpen, setConnectOpen] = useState<boolean>(false);
 
-    
+    const handleOpen = () => {
+        setConnectOpen(true);
+    }
+
 
     return  (
         <Container>
             <SafeAreaView>
-            
+                
+                    <Pressable
+                    style={styles.logout}
+                    onPress={() =>{
+                        console.log("connecting...")
+                        handleOpen()
+                    }}
+                    >
+                        <Text style={{fontSize:18, fontWeight:"bold"}}>Connect to your partner</Text>
+                    </Pressable>
+                    {   connectOpen ? <Text>FORM</Text> :
+                    <View>
+                        <Text>form closed</Text>
+                    </View>
+                    }
+     
                 <Pressable 
                 onPress={()=>  {
                     onLogout
-                    console.log("pushed it")
+                    console.log("logging out")
                 }}
                 
                 style={styles.logout} >
