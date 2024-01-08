04087c10c3baacb84054a4a4dfc7f08439aeefed
Grace Nieboer <grace.nieboer@outlook.com>
2024-01-08 12:41:01 -0500

    activity indicator on a setTimeout after 4 seconds, and display state with an initial example list for testing.

--- components/screens/WhosPaying.tsx
+++ components/screens/WhosPaying.tsx
@@ -1,12 +1,21 @@
 import { FunctionComponent, useState } from "react";
 import { Container } from "../shared/container";
-import { Text, ActivityIndicator, Pressable } from "react-native";
+import { Text, ActivityIndicator, Pressable, View } from "react-native";
 import { colors } from "../shared/colors";
+import { Formik } from 'formik';
 
 
 const WhosPaying:FunctionComponent = () => {
     const [payPerson, setPayPerson] = useState();
     const [isLoading, setIsLoading] = useState<boolean>(false)
+    const [options, setOptions] = useState<String[]>(["noah", "sam", "jack", "grace"]);
+
+    const addAnOption = (newOption:string) => {
+        setOptions([...options, newOption ])
+    }
+    const handlePickPerson = () => {
+
+    }
 
     return  (
         <Container>
@@ -15,13 +24,23 @@ const WhosPaying:FunctionComponent = () => {
              <Pressable
              style={{backgroundColor:`${colors.accent}`, borderRadius:50, padding:10, margin:10}}
              onPress={() => {
-                setIsLoading(!isLoading)
+                setIsLoading(true);
+                setTimeout(() => {
+                    setIsLoading(false);
+                  }, 4000)
 
              }}
              >
                 <Text>PICK FOR US</Text>
              </Pressable>
-           {isLoading ? <ActivityIndicator size="large" /> : <></>} 
+           {isLoading ? <ActivityIndicator size="large" color={colors.accent} /> : <></>} 
+           <View>
+             {options.map(o => {
+                return (
+                    <Text>{o}</Text>
+                )
+             })}
+           </View>
         </Container>
     )
 }
