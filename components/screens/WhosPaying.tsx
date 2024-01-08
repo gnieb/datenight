fdfc8baf92ff4d13615042fe572fc9190fd037e4
Grace Nieboer <grace.nieboer@outlook.com>
2024-01-08 13:07:29 -0500

    formik form added to add a person to the roulette

--- components/screens/WhosPaying.tsx
+++ components/screens/WhosPaying.tsx
@@ -1,6 +1,6 @@
 import { FunctionComponent, useState } from "react";
 import { Container } from "../shared/container";
-import { Text, ActivityIndicator, Pressable, View, StyleSheet } from "react-native";
+import { Text, ActivityIndicator, Pressable, View, StyleSheet, TextInput } from "react-native";
 import { colors } from "../shared/colors";
 import { Formik } from 'formik';
 
@@ -18,10 +18,46 @@ const WhosPaying:FunctionComponent = () => {
         setPayPerson(random)
     }
 
+    const initialValues = {
+        person : ""
+    }
+
     return  (
         <Container>
             <Text
              style={styles.headerView}>So... Who's Paying?</Text>
+             <Formik
+             initialValues={initialValues}
+                onSubmit={(values, {resetForm}) =>  {
+                    console.log(values)
+                    addAnOption(values.person)
+                    resetForm({values: initialValues})
+                }
+                }
+                >
+                    {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
+                <View style={styles.form}>
+                {/* <View style={styles.formContainer}> */}
+                <View style={styles.inputWrapper}>
+                    <TextInput
+                    style={styles.inputStyle}
+                    onChangeText={handleChange('person')}
+                    onBlur={handleBlur('person')}
+                    value={values.person}
+                    placeholder="add someone to the roulette..."
+                    placeholderTextColor={colors.secondary}
+                    />
+                </View>
+                    <Pressable 
+                    onPress={() => {handleSubmit()}} 
+                    style={{ alignItems:"center", backgroundColor:`${colors.secondary}`, borderRadius:50, padding:10, marginBottom:10}}
+                >
+                    <Text style={{color: `${colors.accent}`, fontWeight:"bold", fontSize:18, }}>ADD </Text>
+                </Pressable>
+                </View>
+                // </View>
+                )}
+             </Formik>
              <View>
              {options.map((o, i) => {
                 return (
@@ -78,5 +114,23 @@ const styles = StyleSheet.create({
 
     luckyView: {
         margin: 10
-    }
+    },
+    inputWrapper: {
+        marginBottom: 10,
+     },
+     inputStyle: {
+        borderColor: 'white',
+        fontSize: 20,
+        fontWeight: 'bold',
+        borderWidth: 1,
+        borderRadius: 50,
+        paddingHorizontal: 20,
+        paddingVertical:10,
+        color:'white',
+        backgroundColor:`${colors.accent}`
+     },
+     form: {
+        alignItems: 'center',
+        width:'98%'
+    },
 })
