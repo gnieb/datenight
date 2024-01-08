9dadf99fae53b2a681428b52eecf3add109bbb22
Grace Nieboer <grace.nieboer@outlook.com>
2024-01-08 12:56:37 -0500

    CSS applied to list. Screen now shows list of people options who are in the roulette to pay, the loading bar displays untilm the answer is revealed after 4 seconds. Lucky person is determined y chance with a random function

--- components/screens/WhosPaying.tsx
+++ components/screens/WhosPaying.tsx
@@ -1,12 +1,12 @@
 import { FunctionComponent, useState } from "react";
 import { Container } from "../shared/container";
-import { Text, ActivityIndicator, Pressable, View } from "react-native";
+import { Text, ActivityIndicator, Pressable, View, StyleSheet } from "react-native";
 import { colors } from "../shared/colors";
 import { Formik } from 'formik';
 
 
 const WhosPaying:FunctionComponent = () => {
-    const [payPerson, setPayPerson] = useState();
+    const [payPerson, setPayPerson] = useState<String>("");
     const [isLoading, setIsLoading] = useState<boolean>(false)
     const [options, setOptions] = useState<String[]>(["noah", "sam", "jack", "grace"]);
 
@@ -14,35 +14,69 @@ const WhosPaying:FunctionComponent = () => {
         setOptions([...options, newOption ])
     }
     const handlePickPerson = () => {
-
+        const random = options[Math.floor(Math.random() * (options.length))]
+        setPayPerson(random)
     }
 
     return  (
         <Container>
             <Text
-             style={{color:"white", fontWeight:"bold"}}>So... Who's Paying?</Text>
-             <Pressable
+             style={styles.headerView}>So... Who's Paying?</Text>
+             <View>
+             {options.map((o, i) => {
+                return (
+                    <Text 
+                    key={i}
+                    style={styles.optionPerson}>{o.toUpperCase()}</Text>
+                )
+             })}
+           </View>
+           
+           <Pressable
              style={{backgroundColor:`${colors.accent}`, borderRadius:50, padding:10, margin:10}}
              onPress={() => {
                 setIsLoading(true);
                 setTimeout(() => {
                     setIsLoading(false);
+                    handlePickPerson();
                   }, 4000)
-
+                
              }}
              >
                 <Text>PICK FOR US</Text>
              </Pressable>
-           {isLoading ? <ActivityIndicator size="large" color={colors.accent} /> : <></>} 
-           <View>
-             {options.map((o, i) => {
-                return (
-                    <Text key={i}>{o}</Text>
-                )
-             })}
+           {isLoading ? <ActivityIndicator size="large" color="white" /> : <></>} 
+           
+           <View
+           style={styles.luckyView}
+           >
+            <Text 
+            style={styles.luckyPerson}>{payPerson.toUpperCase()}</Text>
            </View>
         </Container>
     )
 }
 
 export default WhosPaying;
+
+const styles = StyleSheet.create({
+    headerView : {
+        marginBottom: 50,
+        color: "white",
+        fontWeight: "bold", 
+        fontSize:35
+    },
+    optionPerson : {
+        color:"white",
+    },
+    
+    luckyPerson: {
+        fontSize: 30,
+        color: "white",
+        fontWeight: "bold"
+    },
+
+    luckyView: {
+        margin: 10
+    }
+})
