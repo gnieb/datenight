363e398af79813731c8b4263560a6adbc4b0e3b2
Grace Nieboer <grace.nieboer@outlook.com>
2024-01-25 22:15:35 -0500

    Home imagebackground and css

--- components/screens/Home.tsx
+++ components/screens/Home.tsx
@@ -1,9 +1,16 @@
 import { FunctionComponent } from "react";
 import { Container } from "../shared/container";
-import { Text, View } from "react-native";
-import { SafeAreaView, StyleSheet } from "react-native";
+import { Text, View, ImageBackground, SafeAreaView, StyleSheet} from "react-native";
 import { useAuth } from "../context/AuthContext";
+import styled from "styled-components/native";
+import background from "../../assets/photos/blurryholdinghands.avif"
 
+const ImageBackgroundContainer = styled(ImageBackground)`
+  flex: 1;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+`;
 
 const Home: FunctionComponent = () => {
 
@@ -12,12 +19,19 @@ const Home: FunctionComponent = () => {
     return (
         
     <Container>
-        <View style={styles.container}>
-            <Text style={{color:"white"}}>WELCOME {user.first.toUpperCase()}</Text>
-        </View>
+        <ImageBackgroundContainer
+                source={background}
+                style={styles.image}
+                resizeMode="cover"
+                >
+            <View style={styles.container}>
+                <Text style={{color:"white", fontWeight:"bold", fontSize:20}}>HEY {user.first.toUpperCase()}</Text>
+            </View>
+        </ImageBackgroundContainer>
     </Container>
        
     )
+
     
 }
 
@@ -27,5 +41,18 @@ const styles = StyleSheet.create({
     container: {
         flex:1, 
         justifyContent: "center",
-    }
+    },
+    textStyle: {
+           fontSize: 25,
+           fontWeight: 'bold',
+           padding: 10,
+           color:'white',
+           marginBottom: 60,
+           marginTop: 80,
+           margin:50,
+        }, 
+        image: {
+            flex: 1,
+            width: '100%',
+        }
 })
